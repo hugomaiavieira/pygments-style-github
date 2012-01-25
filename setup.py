98645e8a89660bfa52674daa701a7c15de01cae4
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2012-01-25 17:44:04 -0200

    Add keywords

--- setup.py
+++ setup.py
@@ -3,24 +3,25 @@
 from setuptools import setup
 
 setup(
-    name = 'pygments-style-github',
-    version = '0.3',
-    description = 'Pygments version of the github theme.',
-    long_description = open('README.rst').read(),
-    license = 'BSD',
+    name='pygments-style-github',
+    version='0.3',
+    description='Pygments version of the github theme.',
+    long_description=open('README.rst').read(),
+    keywords='pygments style github',
+    license='BSD',
 
-    author = 'Hugo Maia Vieira',
-    author_email = 'hugomaiavieira@gmail.com',
+    author='Hugo Maia Vieira',
+    author_email='hugomaiavieira@gmail.com',
 
-    url = 'https://github.com/hugomaiavieira/pygments-style-github',
+    url='https://github.com/hugomaiavieira/pygments-style-github',
 
-    packages = ['pygments_style_github'],
-    install_requires = ['pygments >= 1.4'],
+    packages=['pygments_style_github'],
+    install_requires=['pygments >= 1.4'],
 
-    entry_points = '''[pygments.styles]
-                      github = pygments_style_github:GithubStyle''',
+    entry_points='''[pygments.styles]
+                    github=pygments_style_github:GithubStyle''',
 
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Plugins',
         'Intended Audience :: Developers',
